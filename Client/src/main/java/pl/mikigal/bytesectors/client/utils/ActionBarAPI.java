d50c1f679c670299c695b893a9315df2a3f6b3aa
mikigal <mikigal.priv@gmail.com>
2020-01-18 19:25:02 +0100

    Added support for MC 1.8+ versions (ActionBar)

--- Client/src/main/java/pl/mikigal/bytesectors/client/utils/ActionBarAPI.java
+++ Client/src/main/java/pl/mikigal/bytesectors/client/utils/ActionBarAPI.java
@@ -10,12 +10,20 @@ import java.lang.reflect.Method;
 public class ActionBarAPI {
 
     private static String nms = Bukkit.getServer().getClass().getPackage().getName().substring(Bukkit.getServer().getClass().getPackage().getName().lastIndexOf(".") + 1);
+    private static boolean useOldMethods = nms.startsWith("v1_8_") || nms.startsWith("v1_7_");
+
     private static Class<?> craftPlayerClass;
     private static Class<?> packetPlayOutChatClass;
     private static Class<?> packetClass;
     private static Class<?> chatSerializerClass;
     private static Class<?> iChatBaseComponentClass;
     private static Method craftPlayerHandleMethod;
+    private static Method m3;
+
+    // 1.8+
+    private static Class<?> chatComponentTextClass;
+    private static Class<?> chatMessageTypeClass;
+    private static Object chatMessageType;
 
     static {
         try {
@@ -25,13 +33,29 @@ public class ActionBarAPI {
             iChatBaseComponentClass = Class.forName("net.minecraft.server." + nms + ".IChatBaseComponent");
             chatSerializerClass = iChatBaseComponentClass.getDeclaredClasses()[0];
             craftPlayerHandleMethod = craftPlayerClass.getDeclaredMethod("getHandle");
+
+            if (useOldMethods) {
+                m3 = chatSerializerClass.getDeclaredMethod("a", String.class);
+            }
+            else {
+                chatComponentTextClass = Class.forName("net.minecraft.server." + nms + ".ChatComponentText");
+                iChatBaseComponentClass = Class.forName("net.minecraft.server." + nms + ".IChatBaseComponent");
+
+                chatMessageTypeClass = Class.forName("net.minecraft.server." + nms + ".ChatMessageType");
+                Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
+
+                for (Object obj : chatMessageTypes) {
+                    if (obj.toString().equals("GAME_INFO")) {
+                        chatMessageType = obj;
+                    }
+                }
+            }
         } catch (ClassNotFoundException | NoSuchMethodException e) {
-            e.printStackTrace();
+            Utils.log(e);
         }
     }
 
     public static void sendActionBar(Player player, String message) {
-
         if (!player.isOnline()) {
             return;
         }
@@ -39,9 +63,17 @@ public class ActionBarAPI {
         ByteSectorsClient.getInstance().getServer().getScheduler().runTaskAsynchronously(ByteSectorsClient.getInstance(), () -> {
             try {
                 Object craftPlayer = craftPlayerClass.cast(player);
-                Method m3 = chatSerializerClass.getDeclaredMethod("a", String.class);
-                Object cbc = iChatBaseComponentClass.cast(m3.invoke(chatSerializerClass, "{\"text\": \"" + Utils.fixColors(message) + "\"}"));
-                Object packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(cbc, (byte) 2);
+                Object packet;
+
+                if (useOldMethods) {
+                    Object cbc = iChatBaseComponentClass.cast(m3.invoke(chatSerializerClass, "{\"text\": \"" + Utils.fixColors(message) + "\"}"));
+                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(cbc, (byte) 2);
+                }
+                else {
+                    Object chatComponentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(Utils.fixColors(message));
+                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, chatMessageTypeClass == null ? byte.class : chatMessageTypeClass}).newInstance(chatComponentText, chatMessageType == null ? ((byte) 2) : chatMessageType);
+                }
+
                 Object craftPlayerHandle = craftPlayerHandleMethod.invoke(craftPlayer);
                 Field playerConnectionField = craftPlayerHandle.getClass().getDeclaredField("playerConnection");
                 Object playerConnection = playerConnectionField.get(craftPlayerHandle);
